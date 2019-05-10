70714611a1319e6f7cf69eda7a34b27f0fd6ceed
Andika Erwansyah <andika.erwansyah@gmail.com>
2019-05-10 07:14:00 +0700

    #4 hapus yang tidak perlu

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -2,17 +2,6 @@
   <v-layout>
     <v-flex text-xs-center>
       <img src="/blackbox-logo.jpeg" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;Blackbox GPS Tracker with Fleetio Template.&#8221;
-        <footer>
-          <small>
-            <em
-              >&mdash; Uji coba clone tampilan Layout dan dashboard Fleetio.com
-              dengan nuxt dan vuetify.js
-            </em>
-          </small>
-        </footer>
-      </blockquote>
     </v-flex>
   </v-layout>
 </template>
