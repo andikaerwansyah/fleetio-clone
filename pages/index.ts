374f794c7cca9de7e54cd931016b95e8568b1f20
Dutek <duhans.id@gmail.com>
2019-05-10 21:54:37 +0700

    remove index.ts lama

--- pages/index.ts
+++
@@ -1,8 +0,0 @@
-import { Vue, Component } from 'nuxt-property-decorator'
-
-@Component
-export default class Index extends Vue {
-  mounted() {
-    this.$router.push({ path: '/dashboard' })
-  }
-}
